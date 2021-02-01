921b8fef60075947119b750a515b8e814bd5c7a5
Sushil Sunuwar <sunuwarsushil_@gmail.com>
2021-02-01 09:59:28 +0800

    OpemEmr String to MediAngel

--- interface/globals.php
+++ interface/globals.php
@@ -476,7 +476,7 @@ if (!empty($glrow)) {
     $GLOBALS['translate_document_categories'] = true;
     $GLOBALS['translate_appt_categories'] = true;
     $GLOBALS['timeout'] = 7200;
-    $openemr_name = 'OpenEMR';
+    $openemr_name = 'MediAngel';
     $css_header = "$web_root/public/themes/style_default.css";
     $GLOBALS['css_header'] = $css_header;
     $compact_header = "$web_root/public/themes/style_default.css";
